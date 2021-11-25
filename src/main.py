463386d1864e1e7e6bdc54aab1dcf2f72a2740f8
eikefab <fabricio.eike.s@gmail.com>
2021-11-25 02:27:31 -0300

    things done

--- src/main.py
+++ src/main.py
@@ -1,107 +1,36 @@
-import models
+from models import init
 from text import *
+from handlers import *
 
 # Iniciando as tabelas
-models.init()
+init()
 
 # Controlando opções
 def main(message: str):
     render(message)
 
-    parent = 0
     option = int(input("> "))
 
-    if option <= 6 or option >= 1: 
-        render_option(option, parent)
+    if option >= 1 and option <= 6: 
+        render_option(option)
 
         parent = option
 
-        option = int(input("> "));
+        option = int(input("> "))
 
-        if option >= 5 or option <= 0: main()
+        if option >= 1 and option < 5:
+            handler = ''
+
+            if parent == 1: handler   =   handle_customer(option)
+            elif parent == 2: handler = handle_employeer(option)
+            elif parent == 3: handler = handle_provider(option)
+            elif parent == 4: handler = handle_category(option)
+            elif parent == 5: handler = handle_product(option)
+            elif parent == 6: handler = handle_cart(option)
+
+            main(handler)
         else:
-            print(" ")
-
-            if parent == 1 or parent == 2:
-                if option == 1:
-                    cpf = int(input("CPF > "))
-                    name = input("Nome > ")
-                
-                    if parent == 2:
-                        role = input("Cargo > ")
-
-                        # TODO: Cadastrar funcionário aqui!
-                        print(cpf, name, role)
-                    else:
-                        # TODO: Cadastrar cliente aqui!
-                        print(cpf, name)
-                
-                    main("Usuário cadastrado com sucesso")
-                elif option == 2:
-                    cpf = int(input("CPF > "))
-
-                    # if parent == 2:
-                        # TODO: Consultar funcionário aqui!
-                    # else:
-                        # TODO: Consultar cliente aqui!
-                    
-                    main("<dados do usuário>")
-                elif option == 3:
-                    cpf = int(input("CPF > "))
-
-                    if parent == 1:
-                        main("Processo não disponível em clientes.")
-                    # else:
-                        # role = input("Cargo > ")
-                        # TODO: Atualizar cargo do funcionário aqui!
-                elif option == 4:
-                    cpf = int(input("CPF > "))
-
-                    # if parent == 1:
-                        # TODO: Deletar o cliente
-                    # elif parent == 2:
-                        # TODO: Demitir o funcionário
-            elif parent == 3:
-                cnpj = int(input("CNPJ > "))
-                name = input("Nome > ")
-
-                # TODO: Cadastrar fornecedor aqui!
-
-                main("Fornecedor cadastrado com sucesso.")
-            elif parent == 4:
-                name = input("Nome > ")
-
-                # TODO: Cadastrar categoria aqui!
-
-                main("Categoria cadastrada com sucesso.")
-            elif parent == 5:
-                name = input("Nome > ")
-                # TODO: Mostrar todas as categorias aqui e esperar ele pegar uma!
-                # TODO: Mostrar todos os fornecedores aqui e esperar ele pegar um!
-                price = int(input("Preço > "))
-                amount = int(input("Quantidade disponível > "))
-
-                # TODO: Cadastrar produto!
-
-                main("Produto cadastrado com sucesso.")
-            elif parent == 6:
-                cpf = input("CPF > ")
-
-                # TODO: Mostrar todos os produtos COM ESTOQUE DISPONÍVEL e 
-                # esperar ele pegar até quando quiser parar!
-
-                # TODO: Calcular preço total
-
-                payment_type = input("Pagamento (Crédito/Débito/Dinheiro) > ").replace('é', 'e').upper()
-
-                while not payment_type in ['CREDITO', 'DEBITO', 'DINHEIRO']:
-                    print("Insira uma opção válida!")
-
-                    payment_type = input("Pagamento (Crédito/Débito/Dinheiro) > ").replace('é', 'e').upper()
-
-                # TODO: Cadastrar carrinho!
-
-                main("Compra feita com sucesso.")
+            main("")
 
 # Executando o programa
 main("")
--- src/text.py
+++ src/text.py
@@ -4,12 +4,14 @@ clear = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
 def title():
     print(" ")
-    print("██████╗░██╗░░░██╗░██████╗██╗░░██╗░█████╗░██████╗░")
-    print("██╔══██╗╚██╗░██╔╝██╔════╝██║░░██║██╔══██╗██╔══██╗")
-    print("██████╔╝░╚████╔╝░╚█████╗░███████║██║░░██║██████╔╝")
-    print("██╔═══╝░░░╚██╔╝░░░╚═══██╗██╔══██║██║░░██║██╔═══╝░")
-    print("██║░░░░░░░░██║░░░██████╔╝██║░░██║╚█████╔╝██║░░░░░")
-    print("╚═╝░░░░░░░░╚═╝░░░╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░░░░")
+    print("  ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+    print("░░░░░░░░██████╗░██╗░░░██╗░██████╗██╗░░██╗░█████╗░██████╗░░░░░░░░░")
+    print("░░░░░░░░██╔══██╗╚██╗░██╔╝██╔════╝██║░░██║██╔══██╗██╔══██╗░░░░░░░░")
+    print("░░░░░░░░██████╔╝░╚████╔╝░╚█████╗░███████║██║░░██║██████╔╝░░░░░░░░")
+    print("░░░░░░░░██╔═══╝░░░╚██╔╝░░░╚═══██╗██╔══██║██║░░██║██╔═══╝░░░░░░░░░")
+    print("░░░░░░░░██║░░░░░░░░██║░░░██████╔╝██║░░██║╚█████╔╝██║░░░░░░░░░░░░░")
+    print("░░░░░░░░╚═╝░░░░░░░░╚═╝░░░╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░░░░░░░░░░░░")
+    print("  ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
     print(" ")
 
 def options():
@@ -20,7 +22,8 @@ def options():
         "4 -> Categoria",
         "5 -> Produto",
         "6 -> Carrinho",
-        "7 -> Sair"
+        "7 -> Sair",
+        " "
     ]
 
     for option in options:
@@ -38,22 +41,21 @@ def render(message: str):
 
     options()
 
-def render_option(option: int, parent: int):
+def render_option(option: int):
     clear()
     title()
 
-    if parent == 0:
-        keys = ['cliente', 'funcionário', 'fornecedor', 'categoria', 'produto', 'carrinho']
+    keys = ['cliente', 'funcionário', 'fornecedor', 'categoria', 'produto', 'carrinho']
 
-        key = keys[option - 1]
+    key = keys[option - 1]
 
-        options = [
-            "1 -> Cadastrar {key}", 
-            "2 -> Consultar {key}", 
-            "3 -> Atualizar {key}", 
-            "4 -> Deletar {key}", 
-            "5 -> Voltar"
-        ]
+    options = [
+        "1 -> Cadastrar {key}", 
+        "2 -> Consultar {key}", 
+        "3 -> Atualizar {key}", 
+        "4 -> Deletar {key}", 
+        "5 -> Voltar"
+    ]
 
-        for option in options:
-            print(option.format(key = key))
+    for option in options:
+        print(option.format(key = key))
